15c8975eba27b442e687b7a2f80fbe2bf3a8138c
wallarelvo <alex@wallar.me>
2015-12-05 18:26:46 -0500

    urghh

---
+++ src/pcl_example.cpp
@@ -0,0 +1,52 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+ros::Publisher pub;
+
+using namespace std;
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input) {
+    // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    pcl::fromROSMsg (*input, cloud);
+
+    pcl::ModelCoefficients coefficients;
+    pcl::PointIndices inliers;
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    // // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setDistanceThreshold (0.01);
+
+    seg.setInputCloud (cloud.makeShared ());
+    seg.segment (inliers, coefficients);
+
+    pcl_msgs::ModelCoefficients ros_coefficients;
+    pcl_conversions::fromPCL(coefficients, ros_coefficients);
+    pub.publish (ros_coefficients);
+}
+
+int main (int argc, char** argv) {
+    ros::init (argc, argv, "pcl_example");
+    ros::NodeHandle nh;
+    pub = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
+    ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
+    ros::spinOnce();
+}
